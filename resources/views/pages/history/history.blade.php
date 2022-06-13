7d0132c5e366d7a3919c363d41ffcd353b5c4b41
nguyencanh2000 <66633212+nguyencanh2k@users.noreply.github.com>
2022-06-13 09:30:43 +0700

    style order

--- resources/views/pages/history/history.blade.php
+++ resources/views/pages/history/history.blade.php
@@ -17,86 +17,89 @@
     </div>
 </section>
 <!-- Breadcrumb Area End -->
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card border-0">
-            <div class="card-body">
-                <h4 class="card-title">Đơn hàng đã đặt</h4>
-                
-                <div class="table-responsive"> 
-                    <?php
-                    $message= Session::get('message');
-                    if($message){
-                        echo '<span class="text-alert text-danger">'.$message.'</span>';
-                        Session::put('message', null);
-                    }
-                    ?>
-                    <table class="table table-bordered table-striped verticle-middle">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Mã đơn hàng</th>
-                                <th scope="col">Thời gian đặt hàng</th>
-                                <th scope="col">Tình trạng đơn hàng</th>
-                                <th scope="col"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $i=0;
-                            @endphp
-                            @foreach ($getorder as $key => $ord)
-                            @php
-                                $i++;
-                            @endphp
-                            <tr>
-                                <td>{{$i}}</td>
-                                <td>{{$ord->order_id}}</td>
-                                <td>{{ $ord->created_at }}</td>
-                                <td>@if($ord->order_status==1)
-                                    <span class="text text-success">Đơn hàng mới</span> 
-                                @elseif($ord->order_status==2)
-                                    <span class="text text-primary">Đã xử lý - Đã giao hàng</span> 
-                                @else
-                                    <span class="text text-danger">Đơn hàng đã bị hủy</span> 
-                                @endif
-                                </td>
-                                <td>
-                                    @if($ord->order_status==1)
-                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#huydon_{{$ord->order_id}}">
-                                        Hủy đơn hàng
-                                    </button>
-                                    @endif
-                                    <form>
-                                        @csrf
-                                        <div class="modal fade" id="huydon_{{$ord->order_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">Lý do hủy đơn hàng</h5>
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                            <span aria-hidden="true">&times;</span>
-                                                        </button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            <textarea id="lydohuydon_{{$ord->order_id}}" cols="110" rows="10"></textarea>
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
-                                                        <button type="button" id="{{$ord->order_id}}" onclick="Huydonhang(this.id)" class="btn btn-success">Gửi</button>
-                                                        </div>
+<div class="cart-main-area mtb-60px">
+    <div class="container">
+        <h3 class="cart-page-title">Đơn hàng đã đặt</h3>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card border-0">
+                    <div class="card-body p-0">
+                        <div class="table-responsive"> 
+                            <?php
+                            $message= Session::get('message');
+                            if($message){
+                                echo '<span class="text-alert text-danger">'.$message.'</span>';
+                                Session::put('message', null);
+                            }
+                            ?>
+                            <table class="table table-bordered table-striped verticle-middle">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Mã đơn hàng</th>
+                                        <th>Thời gian đặt hàng</th>
+                                        <th>Tình trạng đơn hàng</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $i=0;
+                                    @endphp
+                                    @foreach ($getorder as $key => $ord)
+                                    @php
+                                        $i++;
+                                    @endphp
+                                    <tr>
+                                        <td>{{$i}}</td>
+                                        <td>{{$ord->order_id}}</td>
+                                        <td>{{ $ord->created_at }}</td>
+                                        <td>@if($ord->order_status==1)
+                                            <span class="text text-success">Đơn hàng mới</span> 
+                                        @elseif($ord->order_status==2)
+                                            <span class="text text-primary">Đã xử lý - Đã giao hàng</span> 
+                                        @else
+                                            <span class="text text-danger">Đơn hàng đã bị hủy</span> 
+                                        @endif
+                                        </td>
+                                        <td>
+                                            @if($ord->order_status==1)
+                                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#huydon_{{$ord->order_id}}">
+                                                Hủy đơn hàng
+                                            </button>
+                                            @endif
+                                            <form>
+                                                @csrf
+                                                <div class="modal fade" id="huydon_{{$ord->order_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">Lý do hủy đơn hàng</h5>
+                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
+                                                                </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <textarea id="lydohuydon_{{$ord->order_id}}" cols="110" rows="10"></textarea>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                                                                <button type="button" id="{{$ord->order_id}}" onclick="Huydonhang(this.id)" class="btn btn-success">Gửi</button>
+                                                                </div>
+                                                            </div>
                                                     </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <span>
-                                    <a href="{{URL::to('/view-history-order/'.$ord->order_id)}}" data-toggle="tooltip" data-placement="top" title="View">Xem đơn hàng</a>
-                                    </span>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                                </div>
+                                            </form>
+                                            <span>
+                                            <a href="{{URL::to('/view-history-order/'.$ord->order_id)}}" data-toggle="tooltip" data-placement="top" title="View">Xem đơn hàng</a>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/pages/history/review_order.blade.php
+++ resources/views/pages/history/review_order.blade.php
@@ -17,65 +17,69 @@
     </div>
 </section>
 <!-- Breadcrumb Area End -->
-<div class="col-lg-12">
-    <div class="card border-0">
-        <div class="card-body">
-            <h3 class="card-title mb-4">Đánh giá sản phẩm</h3>
-            
-            <div class="table-responsive"> 
-                @if(session()->has('message'))
-                    <div class="alert alert-success">
-                        {!! session()->get('message') !!}
-                    </div>
-                @elseif(session()->has('error'))
-                    <div class="alert alert-danger">
-                        {!! session()->get('error') !!}
-                    </div>
-                @endif
-                @foreach($product_review as $key => $prd)
-                <div class="col-lg-12">
-                    <div class="ratting-form-wrapper" style="padding-left: 0px">
-                        <div class="single-review">
-                            <div class="review-img">
-                                <img height="120px" width="120px" src="{{URL::to('public/uploads/product/'.$prd->product->product_image)}}" alt="" />
+<div class="cart-main-area mtb-60px">
+    <div class="container">
+        <div class="col-lg-12 p-0">
+            <div class="card border-0">
+                <div class="card-body p-0">
+                    <h4 class="card-title mb-4">Đánh giá sản phẩm</h4>
+                    
+                    <div class="table-responsive"> 
+                        @if(session()->has('message'))
+                            <div class="alert alert-success">
+                                {!! session()->get('message') !!}
                             </div>
-                            <div class="review-content">
-                                <div class="review-top-wrap">
-                                    <div class="review-left">
-                                        <div class="review-name">
-                                            <h3>{{$prd->product->product_name}}</h3>
+                        @elseif(session()->has('error'))
+                            <div class="alert alert-danger">
+                                {!! session()->get('error') !!}
+                            </div>
+                        @endif
+                        @foreach($product_review as $key => $prd)
+                        <div class="col-lg-12 border border-secondary rounded p-4 mb-4">
+                            <div class="ratting-form-wrapper" style="padding-left: 0px">
+                                <div class="single-review">
+                                    <div class="review-img">
+                                        <img height="120px" width="120px" src="{{URL::to('public/uploads/product/'.$prd->product->product_image)}}" alt="" />
+                                    </div>
+                                    <div class="review-content">
+                                        <div class="review-top-wrap">
+                                            <div class="review-left">
+                                                <div class="review-name">
+                                                    <a href="{{URL::to('/chi-tiet-san-pham/'.$prd->product->product_id)}}"><h3>{{$prd->product->product_name}}</h3></a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                                <div class="ratting-form">
+                                    <form action="{{URL::to('/add-review')}}" method="post">
+                                        @csrf
+                                        <div class="star-box">
+                                            <span>Đánh giá sao:</span>
+                                            <div class="rateYo"></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="rating-form-style form-submit">
+                                                    @php
+                                                        $customer_id = Session::get('customer_id')
+                                                    @endphp
+                                                    <input type="hidden" name="product_id" value="{{$prd->product_id}}">
+                                                    <input type="hidden" name="order_id" value="{{$prd->order_id}}">
+                                                    <input type="hidden" name="customer_id" value="{{$customer_id}}">
+                                                    <input type="hidden" name="rating" class="rating">
+                                                    <textarea name="comment" placeholder="Vui lòng viết đánh giá sản phẩm" required></textarea>
+                                                    <input type="submit" value="Gửi" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                        <div class="ratting-form">
-                            <form action="{{URL::to('/add-review')}}" method="post">
-                                @csrf
-                                <div class="star-box">
-                                    <span>Đánh giá sao:</span>
-                                    <div class="rateYo"></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="rating-form-style form-submit">
-                                            @php
-                                                $customer_id = Session::get('customer_id')
-                                            @endphp
-                                            <input type="hidden" name="product_id" value="{{$prd->product_id}}">
-                                            <input type="hidden" name="order_id" value="{{$prd->order_id}}">
-                                            <input type="hidden" name="customer_id" value="{{$customer_id}}">
-                                            <input type="hidden" name="rating" class="rating">
-                                            <textarea name="comment" placeholder="Vui lòng viết đánh giá sản phẩm" required></textarea>
-                                            <input type="submit" value="Gửi" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
-                @endforeach
             </div>
         </div>
     </div>
